67a8e245aa795d8a6f6e7c8141c1daddf004c8eb
Ferenc Hammerl <ferenc.hammerl@orderyoyo.com>
2019-04-24 15:38:05 +0200

    Always use latest package

--- pageSpeedTask/tests/_suite.ts
+++ pageSpeedTask/tests/_suite.ts
@@ -16,20 +16,35 @@ describe('Sample task tests', function () {
     //     // Add success test here
     // });
 
-    it('it should fail', function (done: MochaDone) {
-        this.timeout(1000);
+    it('it should return print a number between 0 and 1', function (done: MochaDone) {
+        this.timeout(20000);
 
-        let tp = path.join(__dirname, 'failure.js');
+        let tp = path.join(__dirname, 'success.js');
         let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
 
         tr.run();
-        console.log(tr.succeeded);
-        assert.equal(tr.succeeded, false, 'should have failed');
+        console.log(tr.stdout);
+        assert.equal(tr.succeeded, true, 'should have succeded');
         assert.equal(tr.warningIssues, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.equal(tr.errorIssues[0], 'Not implemented, should alawys fail, input was microsoft.com, wikipedia.org', 'error issue output');
-        assert.equal(tr.stdout.indexOf('Hello bad'), -1, "Should not display Hello bad");
-
+        assert.equal(tr.errorIssues.length, 0, "should have 0 error issues");
+        assert.equal(tr.stdout.split(' ').filter(x => x.match(/(0(\.\d+)?|1(\.0+)?)/)).length > 0, true, "Should contain one number");
         done();
     });
+
+    // it('it should fail', function (done: MochaDone) {
+    //     this.timeout(1000);
+
+    //     let tp = path.join(__dirname, 'failure.js');
+    //     let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+    //     tr.run();
+    //     console.log(tr.succeeded);
+    //     assert.equal(tr.succeeded, false, 'should have failed');
+    //     assert.equal(tr.warningIssues, 0, "should have no warnings");
+    //     assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
+    //     assert.equal(tr.errorIssues[0], 'Not implemented, should alawys fail, input was microsoft.com, wikipedia.org', 'error issue output');
+    //     assert.equal(tr.stdout.indexOf('Hello bad'), -1, "Should not display Hello bad");
+
+    //     done();
+    // });
 });
--- pageSpeedTask/tests/success.ts
+++ pageSpeedTask/tests/success.ts
@@ -4,6 +4,8 @@ import path = require('path');
 let taskPath = path.join(__dirname, '..', 'index.js');
 let tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
-tmr.setInput('urllist', 'microsoft.com, wikipedia.org');
+tmr.setInput('urlList', 'https://microsoft.com, https://wikipedia.org');
+tmr.setInput('apiKey', '');
+tmr.setInput('requestDelay', '2000');
 
 tmr.run();
